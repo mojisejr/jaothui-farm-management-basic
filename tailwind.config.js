a0ffa6cd4ce110710b304c30963e590a89a6aad4
nonthasak laoluerat <non@MacBook-Air-khxng-nonthasak.local>
2025-06-30 00:29:10 +0700

    upd: task ID 1 completed

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'jaothui': '#D4AF37',
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: ["light", "dark", "cupcake",
+      {
+        jaothui: {
+          "primary": "#D4AF37",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+}; 
